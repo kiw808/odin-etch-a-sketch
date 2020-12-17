52380b02c330bc82940e0d349c6236dbfc5a7f0e
Pierre Muller <pierremanu.muller@gmail.com>
2020-12-17 18:22:18 +0100

    hover change opacity

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ class GridSystem {
 
     resetGrid () {
         let grid = this.gridWrapper.querySelectorAll('.grid-item');
+        
         grid.forEach(element => {
             element.remove();
         })
@@ -18,15 +19,20 @@ class GridSystem {
 
     createGrid (numberOfItems) {
         this.resetGrid();
-        
+
         this.gridWrapper.setAttribute('style', 'grid-template-columns: repeat(' + this.gridSide + ', 1fr);')
 
         for (let i = 0; i < numberOfItems; i++) {
             let gridElement = document.createElement('div');
             gridElement.classList.add('grid-item');
             gridElement.setAttribute('id', 'item-' + (i + 1));
+
+            gridElement.addEventListener('mouseleave', () => {
+                gridElement.style.opacity = 0.5; 
+                console.log(gridElement.style.opacity);
+            })
+
             this.gridWrapper.appendChild(gridElement);
-            console.log(gridElement);
         }
     }
 }
